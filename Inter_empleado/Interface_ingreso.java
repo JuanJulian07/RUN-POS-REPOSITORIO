3b7f5e6377ba23c81fa1d6ce05cdbb5380d76c23
juan2702-max <jbeltrano@unal.edu.co>
2023-05-16 21:39:09 -0500

    Inicio de la clase interface Usuario, especificamente interface de ingreso

--- Inter_empleado/Interface_ingreso.java
+++ Inter_empleado/Interface_ingreso.java
@@ -2,11 +2,21 @@ package Inter_empleado;
 
 import javax.swing.*;
 
+
 public class Interface_ingreso extends JFrame{
     
-    protected Interface_ingreso(){
+    protected String usuario;
+    protected String contrasñea;
+    protected JPanel panel;
+
+
+    public Interface_ingreso(){
         super("Ventana de ingreso");
         this.setSize(800,800);
+        panel = new JPanel();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(panel);
+        setVisible(true);
 
     }
     
