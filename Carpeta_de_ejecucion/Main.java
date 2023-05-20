99227f02ef842097851ff63c0db26dd31bd2f5fb
juan2702-max <jbeltrano@unal.edu.co>
2023-05-19 20:51:19 -0500

    dejando la clase main limpia

--- Carpeta_de_ejecucion/Main.java
+++ Carpeta_de_ejecucion/Main.java
@@ -11,15 +11,6 @@ public class Main {
         //https://lucid.app/lucidchart/e688b790-61e5-4f3c-b4fe-9bc67ce1aa03/edit?invitationId=inv_fa273c72-f1d5-47cb-a82a-f0f1505e27bd&page=0GSVd8AeXx9V#
         
         
-        Scanner t = new Scanner (System.in);
-        Empleado am = null;
-        String nombre = t.nextLine(); 
-        String documento = t.nextLine();
-
         
-        Leer_empleados emplea = new Leer_empleados(nombre, documento);
-
-        System.out.println(emplea.get_acceso());
-        am = emplea.get_empleado();
     }
 }
--- Leer_Bases_de_datos/Leer_empleados.java
+++ Leer_Bases_de_datos/Leer_empleados.java
@@ -6,9 +6,10 @@ import java.io.*;
 
 public class Leer_empleados {
     
-    private Empleado em = null;
-    private String dir = new String("Bases_de_datos\\Base_empleados.csv");
-    private boolean acceso = false;
+    //Atributos
+    private Empleado em = null; //Guarda el empleado encontrado
+    private String dir = new String("Bases_de_datos\\Base_empleados.csv"); //Esta es la direccion por defecto del archivo de la base de datos
+    private boolean acceso = false; //Sirve para verificar si podemos obtener acceso
 
 
     public Leer_empleados(String usuario, String contra){
@@ -18,11 +19,11 @@ public class Leer_empleados {
 
             while(be.hasNextLine()){
                 String datos_empleado = be.nextLine();
-                String aux[] = datos_empleado.split(";");
+                String aux[] = datos_empleado.split(";"); //Obtenemos un arreglo de Strings donde tenemos por separado cada tipo de dato
 
                 Empleado emaux = null;
 
-                if(is_empleado(aux[3], aux[4], usuario, contra)){
+                if(is_empleado(aux[3], aux[4], usuario, contra)){//Verificamos que si coincida algun usuario y procedemos a guardarlo dependiendo de su rol
                     switch(aux[2]){
                         case "Gerente": emaux = new Gerente(aux[1],Long.parseLong(aux[0]), aux[3], aux[4]); break;
                         case "Administrativo": emaux = new Administrativo(aux[1],Long.parseLong(aux[0]), aux[3], aux[4]); break;
@@ -36,14 +37,14 @@ public class Leer_empleados {
             }
 
         }
-        catch(IOException em){
-
-            String menaje = new String ("No encontramos el archivo Base_empleados.csv en la direccion " + dir);
-
-            System.out.print("nel");
+        catch(IOException exepcion){
+            //Falta implementar un throw para la interface grafica
+            String mensaje = new String ("No encontramos el archivo Base_empleados.csv en la direccion " + dir);
+            
         }
     }
 
+    //Este metodo nos reestablece la variable booleana acceso
     private boolean is_empleado(String bsusuario, String bscontra, String usuario, String contra){
 
         if(bsusuario.equals(usuario)){
@@ -55,11 +56,11 @@ public class Leer_empleados {
         return acceso;
     }
 
-    public boolean get_acceso(){
+    public boolean get_acceso(){//retorna el valor de nuestra variable acceso
         return acceso;
     }
 
-    public Empleado get_empleado(){
+    public Empleado get_empleado(){//retorna el empleado que fue encontrado segun sea el caso
         if(acceso == true){
             return em;
         }
