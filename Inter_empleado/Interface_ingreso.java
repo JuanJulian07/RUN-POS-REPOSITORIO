58ce7b80cc833bc9435d1d9eee7daf207eafda00
juan2702-max <jbeltrano@unal.edu.co>
2023-05-19 19:32:33 -0500

    Creacion de la clase Leer empleados

--- Inter_empleado/Interface_ingreso.java
+++ Inter_empleado/Interface_ingreso.java
@@ -1,6 +1,7 @@
 package Inter_empleado;
 
 import javax.swing.*;
+import Empleados.Empleado;
 
 
 public class Interface_ingreso extends JFrame{
@@ -8,16 +9,26 @@ public class Interface_ingreso extends JFrame{
     protected String usuario;
     protected String contrasñea;
     protected JPanel panel;
+    protected Empleado user;
 
 
     public Interface_ingreso(){
         super("Ventana de ingreso");
         this.setSize(800,800);
-        panel = new JPanel();
+        panel = Panel_ingreso();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //Aprender lamdas para hacer el boton y repasar interfaces
         add(panel);
+        pack();
         setVisible(true);
 
+    }
+
+    protected JPanel Panel_ingreso(){
+        //Retorna la configuracion del panel o la distribucion principalmente para el ingreso del personal con todos los botones
+        JPanel p = new JPanel(/*Ingresar el layout */);
+        
+        return p;
     }
     
 }
