5c6f728d8008a75c6d2711402ae50ae2b9588476
juan2702-max <jbeltrano@unal.edu.co>
2023-05-26 14:51:27 -0500

    Verificando que interfaces le compete a cada empleado

--- src/Carpeta_de_ejecucion/Main.java
+++ src/Carpeta_de_ejecucion/Main.java
@@ -15,12 +15,12 @@ public class Main {
         //Enlace de los diagramas
         //https://lucid.app/lucidchart/e688b790-61e5-4f3c-b4fe-9bc67ce1aa03/edit?invitationId=inv_fa273c72-f1d5-47cb-a82a-f0f1505e27bd&page=0GSVd8AeXx9V#
         
-        Empleado usuario = null;
+        Empleado empleado = null;
         Interface_ingreso ingreso = new Interface_ingreso();
         
         
-        while(usuario == null){
-            usuario = ingreso.get_empleado();
+        while(empleado == null){
+            empleado = ingreso.get_empleado();
             try{
                 Thread.sleep(100);
                 
@@ -32,8 +32,21 @@ public class Main {
         }
         
         ingreso = null;
-        Interface_Gerente gerente = new Interface_Gerente(usuario);
-        
-        
+
+        if(empleado instanceof Gerente){
+            Interface_Gerente gerente = new Interface_Gerente(empleado);
+        }
+        if(empleado instanceof Administrativo){
+            //Implementar interface grafica
+            System.out.println("admin");
+        }
+        if(empleado instanceof Cocinero){
+            //Implementar interface grafica
+            System.out.println("concinero");
+        }
+        if(empleado instanceof Mesero){
+            //Implementar interface grafica
+            System.out.println("mesero");
+        }
     }
 }
