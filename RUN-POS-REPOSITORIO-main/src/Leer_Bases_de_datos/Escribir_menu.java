975fa8efa8493417453701351202d0fe3f3ab05f
juan2702-max <jbeltrano@unal.edu.co>
2023-06-16 22:29:17 -0500

    Integracion de la clase escribir menu

---
+++ RUN-POS-REPOSITORIO-main/src/Leer_Bases_de_datos/Escribir_menu.java
@@ -0,0 +1,23 @@
+package Leer_Bases_de_datos;
+import java.util.ArrayList;
+import Menu.Menu;
+import javax.swing.JOptionPane;
+import java.io.*;
+
+
+public class Escribir_menu {
+    private static String dir = new String ("src\\Bases_de_datos\\Base_menu.csv");
+    public Escribir_menu(ArrayList<Menu> menu){
+        //Esto solo se encargara de guardar la base de datos y listo xd
+        try{
+            PrintWriter escribir = new PrintWriter(new FileWriter(dir, false));
+            for(Menu aux : menu){
+                escribir.print(aux.get_formatio_bd());
+            }
+            escribir.close();
+        }
+        catch(IOException e){
+            JOptionPane.showMessageDialog(null, "No pudimos encontrar la base de datos Menu", "Error base de datos", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+}
